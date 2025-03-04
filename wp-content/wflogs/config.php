5a5923fc567e16e064395f5ebf53345b841710cc
Yevhen Nikulin <yevhen.nikulin@kitrum.com>
2025-03-04 18:50:59 +0200

    Initial Commit

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1741711714;s:7:"authKey";s:64:"#r8P22]?%p)XY5/}2-~1?nDk$CinA5H9erh{*w}`droE wCf{W6zT`<1tKtEhn/{";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:1;s:13:"attackDataKey";i:3977;}
