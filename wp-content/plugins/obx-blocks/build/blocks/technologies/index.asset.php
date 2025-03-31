ab1d2ebf6e1801fa26409751a26f1577505223cb
Yevhen Nikulin <yevhen.nikulin@kitrum.com>
2025-03-31 10:31:53 +0300

    Small fix for techs block

--- wp-content/plugins/obx-blocks/build/blocks/technologies/index.asset.php
+++ wp-content/plugins/obx-blocks/build/blocks/technologies/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '69942647c9f9ad092825');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '29d9b1d38f4a7c7d7160');
